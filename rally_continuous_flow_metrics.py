e230958e93c5f27d4d4630745b099d3de6b6adc0
Adeel Ali <adeel.ali@clickchain.com>
2019-08-15 14:52:22 -0400

    Improved the script

--- rally_continuous_flow_metrics.py
+++ rally_continuous_flow_metrics.py
@@ -4,10 +4,9 @@ import csv
 import pendulum
 import logging
 import yaml
+import os
 
 logging.getLogger().setLevel(logging.INFO)
-report_start_date = "2019-07-21"
-report_end_date = "2019-08-03"
 
 
 def publish_continuous_flow_metrics():
@@ -16,59 +15,68 @@ def publish_continuous_flow_metrics():
 
 
 def find_deployed_stories():
-    deployed_stories = []
-    fields = "FormattedID,ScheduleState,PlanEstimate,State,Name,CreationDate,RevisionHistory,Revisions,FlowState"
-    query = "CreationDate >= 2019-05-01"
-    stories = rally_instance().get('UserStory', fields=fields, query=query, instance=True)
     existing_flow_states = find_flow_state_names()
-    for story in stories:
-        if check_time_range(story):
-            logging.info("making story object for " + story.FormattedID)
-            deployed_stories.append(Story(story, existing_flow_states))
+    report_start_date = "2019-01-01"
+    report_end_date = "2019-08-15"
+    rally_configuration = RallyConfiguration()
+    deployed_stories = list()
+    fields = "FormattedID, ScheduleState, PlanEstimate, State, " \
+             "Name, CreationDate, RevisionHistory, Revisions, FlowState"
+    query = "CreationDate >= " + rally_configuration.story_creation_start_date().__str__()
+    rally_stories = rally_instance().get('UserStory', fields=fields, query=query, instance=True)
+    for rally_story in rally_stories:
+        #if check_time_range(rally_story, report_start_date, report_end_date):
+        deployed_stories.append(Story(rally_story, existing_flow_states))
     return deployed_stories
 
 
 def find_flow_state_names():
     flow_states = rally_instance().get("FlowState")
-    logging.info("getting flow states")
     flow_states_names = []
     for flow_state in flow_states:
         flow_states_names.append(flow_state.Name)
     return flow_states_names
 
 
-def check_time_range(story):
+def check_time_range(story, report_start_date, report_end_date):
     for revision in story.RevisionHistory.Revisions:
-        if Parser.parse_line_check_state(revision.Description, rally_cycle_time_end_state()):
+        if Parser.parse_line_check_state(revision.Description, RallyConfiguration().cycle_time_end_state()):
             if pendulum.parse(report_start_date) <= pendulum.parse(revision.CreationDate) <= pendulum.parse(
                     report_end_date):
                 return True
 
 
 def write_to_csv_file(stories):
-    logging.info("creating CSV")
-    with open("rally_metrics.csv", 'w', newline='') as csv_file:
+    with open("reports/" + RallyConfiguration().project_name().replace(" ", "_").lower() + "_rally_metrics.csv", 'w', newline='') as csv_file:
         output = csv.writer(csv_file)
-        output.writerow(["Story ID", "Story", "Doing", "Done", "Cycle Time"])
+        output.writerow(["STORY ID", "Story DESCRIPTION", "IN-PROGRESS", "COMPLETED",
+                         "DEPLOYED TO STAGE/PERF",  "DONE [DEPLOYED TO PROD]", "CYCLE TIME"])
         for story in stories:
-            output.writerow([story.id, story.name, story.revisions["revisions"].get(rally_cycle_time_start_state()),
-                             story.revisions["revisions"].get(rally_cycle_time_end_state()), story.cycle_time()])
-        logging.info("CSV created")
+            output.writerow([story.id,
+                             story.name,
+                             story.flow_state_changes.get(RallyConfiguration().cycle_time_start_state()),
+                             story.flow_state_changes.get('COMPLETED'),
+                             story.flow_state_changes.get('DEPLOYED TO STAGE/PERF'),
+                             story.flow_state_changes.get(RallyConfiguration().cycle_time_end_state()),
+                             story.cycle_time])
 
 
 def rally_instance():
+    os.environ.get('HTTPS_PROXY', None)
     rally_configuration = RallyConfiguration()
     return Rally(server=rally_configuration.server_uri(),
                  apikey=rally_configuration.api_key(),
                  project=rally_configuration.project_name(),
-                 workspace=rally_configuration.work_space())
+                 workspace=rally_configuration.work_space(),
+                 )
 
 
 class Parser:
 
     @staticmethod
     def parse_line_check_state(line, state):
-        revision_change_regular_expression = re.compile(r'([A-Z ]+) changed from \[(.*)\] to \[' + state + '\]')
+        state = re.escape(state)
+        revision_change_regular_expression = re.compile(r'([A-Z ]+) STATE changed from \[(.*)\] to \[' + state + '\]')
         match = revision_change_regular_expression.search(line)
         if match:
             return True
@@ -81,30 +89,31 @@ class Revisions:
 
 
 class Story:
-    revisions = {}
 
-    def __init__(self, story, flow_states):
-        self.revisions["revisions"] = self.create_revision_based_on_state(flow_states, story.RevisionHistory.Revisions)
-        self.name = story.Name
-        self.id = story.FormattedID
-        self.schedule_state = story.ScheduleState
-        self.points = story.PlanEstimate
+    def __init__(self, rally_story, flow_states):
+        self.name = rally_story.Name
+        self.id = rally_story.FormattedID
+        self.schedule_state = rally_story.ScheduleState
+        self.points = rally_story.PlanEstimate
+        self.flow_state_changes = self.get_flow_state_changes(rally_story.RevisionHistory.Revisions, flow_states)
+        self.cycle_time = self.get_cycle_time()
 
-    @staticmethod
-    def create_revision_based_on_state(flow_states, revisions):
-        temp = {}
+    def get_flow_state_changes(self, rally_revisions, flow_states):
+        flow_state_changes = {}
         for item in flow_states:
-            for revision in revisions:
+            for revision in rally_revisions:
                 if Parser.parse_line_check_state(revision.Description, item):
-                    temp[item] = revision.CreationDate
-        return temp
+                    flow_state_changes[item] = revision.CreationDate
+        return flow_state_changes
 
-    def cycle_time(self):
-        result = ""
-        if self.revisions["revisions"].get(rally_cycle_time_start_state()) is not None \
-                and self.revisions["revisions"].get(rally_cycle_time_end_state()) is not None:
-            temp = pendulum.parse(self.revisions["revisions"].get(rally_cycle_time_end_state())) - pendulum.parse(
-                self.revisions["revisions"].get(rally_cycle_time_end_state()))
+    def get_cycle_time(self):
+        result = ''
+        start_state = RallyConfiguration().cycle_time_start_state()
+        end_state = RallyConfiguration().cycle_time_end_state()
+        if self.flow_state_changes.get(start_state) is not None \
+                and self.flow_state_changes.get(end_state) is not None:
+            temp = pendulum.parse(self.flow_state_changes.get(end_state)) - pendulum.parse(
+                self.flow_state_changes.get(start_state))
             result = str(temp._days)
         return result
 
@@ -116,7 +125,7 @@ class RallyConfiguration:
     def __init__(self):
         with open("rally_config.yml", 'r') as stream:
             try:
-                self.rally_configuration = yaml.safe_load(stream)
+                self.rally_configuration = yaml.safe_load(stream)["rally"]
             except yaml.YAMLError as exc:
                 print(exc)
 
@@ -138,6 +147,14 @@ class RallyConfiguration:
     def cycle_time_end_state(self):
         return self.rally_configuration["board"]["cycleTime"]["endState"]
 
+    def story_creation_start_date(self):
+        return self.rally_configuration["board"]["story"]["creationDate"]
+
+    def proxy(self):
+        return self.rally_configuration["proxy"]
+
 
 if __name__ == "__main__":
+    if RallyConfiguration().proxy() is not None:
+        os.environ['HTTPS_PROXY'] = "proxyvipecc.nb.ford.com:83"
     publish_continuous_flow_metrics()
--- rally_continuous_flow_metrics_tests.py
+++ rally_continuous_flow_metrics_tests.py
@@ -3,9 +3,14 @@ import rally_continuous_flow_metrics
 
 class MyTestCase(unittest.TestCase):
 
-    def when_deployed_state_parse_successfully(self):
-        line='FLOW STATE CHANGED DATE changed from [Fri Aug 02 08:36:58 MDT 2019] to [Mon Aug 05 13:29:00 MDT 2019], FLOW STATE changed from [Ready For Prod] to [Deployed]'
-        state='Deployed'
+    def test_when_deployed_state_then_parse_successfully(self):
+        line = 'FLOW STATE CHANGED DATE changed from [Fri Aug 02 08:36:58 MDT 2019] to [Mon Aug 05 13:29:00 MDT 2019], FLOW STATE changed from [Ready For Prod] to [Deployed]'
+        state = 'Deployed'
+        self.assertEqual(True, rally_continuous_flow_metrics.Parser.parse_line_check_state(line, state))
+
+    def test_when_deployed_state_name_has_special_characters_then_parse_successfully(self):
+        line = 'FLOW STATE CHANGED DATE changed from [Tue Jul 02 13:51:45 MDT 2019] to [Fri Aug 09 14:31:57 MDT 2019], FLOW STATE changed from [DEPLOYED TO STAGE/PERF] to [DONE [DEPLOYED TO PROD]]'
+        state = rally_continuous_flow_metrics.RallyConfiguration().cycle_time_end_state()
         self.assertEqual(True, rally_continuous_flow_metrics.Parser.parse_line_check_state(line, state))
 
 
